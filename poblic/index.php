a6f0c8cc2c9fe6cc0aa9605d3fbcc3872edb7f14
nazisharifi2025 <banonazisharifi@gmail.com>
2025-08-06 13:59:08 +0430

    l

--- poblic/index.php
+++ poblic/index.php
@@ -7,10 +7,14 @@
     <link rel="stylesheet" href="../src/output.css">
 </head>
 <!-- npx @tailwindcss/cli -i ./src/input.css -o ./src/output.css --watch -->
-<body class="h-screen w-full flex justify-center items-center flex-col">
-    <div class="w-[27%] h-fit flex flex-col ">
+<body class="h-screen w-full  flex justify-center items-center flex-col">
+        <form action="" class="w-[27%] h-fit gap-3 flex flex-col ">
         <input type="text" class="w-full py-3 rounded border-2 border-gray-300 bg-gray-100">
         <input type="text" class="w-full py-3 rounded border-2 border-gray-300 bg-gray-100">
-    </div>
+        <button class="bg-blue-400 rounded w-full py-3 text-white font-bold">Log in</button>
+        <div class="flex justify-center items-center space-x-4">
+
+        </div>
+        </form>
 </body>
 </html>
