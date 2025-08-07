75e58916534cecacad697b55dd216544568e7cae
Nazi <banonazisharifi@gmail.com>
2025-08-07 21:38:05 +0430

    Label

--- poblic/Home.php
+++ poblic/Home.php
@@ -9,7 +9,10 @@
 <body class="h-screen w-full bg-pink-100 flex justify-center items-center">
     <div class="w-[90%] h-[90%] shadow-md shadow-gray-500 flex justify-between">
         <form action="" class="w-1/2 h-full flex flex-col ">
+            <label for="">
+                <span class="font-bold text-xl">Name</span>
             <input type="text" class="shadow-md shadow-gray-500 w-full py-3">
+            </label>
         </form>
     </div>
 </body>
