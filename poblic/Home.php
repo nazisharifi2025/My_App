7059732df5cdeea213a7ed59f6a890496695d355
Nazi <banonazisharifi@gmail.com>
2025-08-08 22:57:33 +0430

    Login page

--- poblic/Home.php
+++ poblic/Home.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet" href="../src/output.css">
 </head>
 <body class="h-screen w-full  bg-linear-180 from-pink-500 to-violet-950 flex justify-center items-center">
-    <div class="w-[90%] h-[90%] shadow-md shadow-gray-500 flex justify-between">
+    <div class="w-[90%] h-[90%] shadow-md shadow-gray-500 flex-col  flex justify-between">
         <form action="" class="w-1/2 h-full flex flex-col ">
             <label for="">
                 <span class="font-bold text-xl">Name</span>
@@ -27,6 +27,7 @@
             </label>
             <button class="py-2 px-3 bg-blue-400 rounded-xl">Sign Up</button>
         </form>
+        <div class="py-3 px-4 rounded-2xl rounded-t-none w-[40%]"></div>
     </div>
 </body>
 </html>
