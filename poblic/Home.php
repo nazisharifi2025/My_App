16f7e1d0bacc67f02339b4440c0ae20ae17d6df4
Nazi <banonazisharifi@gmail.com>
2025-08-08 23:28:26 +0430

    Login page

--- poblic/Home.php
+++ poblic/Home.php
@@ -8,7 +8,7 @@
 </head>
 <body class="h-screen w-full  bg-linear-180 from-pink-500 to-violet-950 flex justify-center items-center">
     <div class="w-[90%] h-[90%] shadow-md shadow-gray-500 flex-col  flex justify-between">
-        <form action="" class="w-1/2 h-full flex flex-col ">
+        <form action="" class="w-1/2 h-full flex flex-col bg-gray-200 rounded-2xl">
             <label for="">
                 <span class="font-bold text-xl">Name</span>
             <input type="text" class="shadow-md shadow-gray-500 border rounded-xl w-full py-3">
