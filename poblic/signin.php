943455741dd8f3c770bac8f3c15dde0a0edb526e
Nazi <banonazisharifi@gmail.com>
2025-08-09 22:53:58 +0430

    One style added

--- poblic/signin.php
+++ poblic/signin.php
@@ -9,7 +9,7 @@
 <body class="h-screen w-full bg-gray-100 text-2xl text-black flex justify-center items-center flex-col">
     <div class="w-1/2 h-[80%] border border-gray-400 rounded-2xl shadow-md shadow-gray-600 flex flex-col gap-2.5 p-4 justify-between">
         <h1 class="font-bold text-xl text-center">Login Page</h1>
-        <input type="text" class="px-2 py-2 border border-gray-600 ">
+        <input type="text" class="px-2 py-2 border border-gray-600 bg-gray-100 font-bold">
     </div>
 </body>
 </html>
