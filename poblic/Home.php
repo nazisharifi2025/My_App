76b99fd561f7316651fc19aad69f1fe2b4b791b5
Nazi <banonazisharifi@gmail.com>
2025-08-10 11:02:53 +0430

    One input added

--- poblic/Home.php
+++ poblic/Home.php
@@ -10,22 +10,26 @@
     <div class="w-[90%] h-[90%] shadow-md shadow-gray-500  bg-gray-900 ">
         <h1 class="font-bold text-2xl text-center">Sign Up</h1>
     <div class="flex justify-between h-full w-full">
-        <form action="" class="w-1/2 h-full flex flex-col justify-between py-12 rounded-2xl">
+        <form class="w-1/2 h-full flex flex-col justify-between  rounded-2xl">
+            <label for="">
+                <span class="font-bold text-xl">Last Name</span>
+            <input type="text" name="LastName" class="shadow-md shadow-gray-500 border rounded-xl w-full py-3">
+            </label>
             <label for="">
                 <span class="font-bold text-xl">Name</span>
-            <input type="text" class="shadow-md shadow-gray-500 border rounded-xl w-full py-3">
+            <input type="text" name="Name" class="shadow-md shadow-gray-500 border rounded-xl w-full py-3">
             </label>
             <label for="">
                 <span class="font-bold text-xl">Email</span>
-            <input type="email" class="shadow-md shadow-gray-500 border rounded-xl w-full py-3">
+            <input type="email" name="Email" class="shadow-md shadow-gray-500 border rounded-xl w-full py-3">
             </label>
             <label for="">
                 <span class="font-bold text-xl">Password</span>
-            <input type="password" class="shadow-md shadow-gray-500 border rounded-xl  w-full py-3">
+            <input type="password" name="Password1" class="shadow-md shadow-gray-500 border rounded-xl  w-full py-3">
             </label>
             <label for="">
                 <span class="font-bold text-xl">Confirm Password</span>
-            <input type="password" class="shadow-md shadow-gray-500  rounded-xl  w-full py-3">
+            <input type="password" name="password2" class="shadow-md shadow-gray-500  rounded-xl  w-full py-3">
             </label>
             <div class="w-full flex justify-between font-bold text-xs">
                 <span>Forgit Password?</span>
