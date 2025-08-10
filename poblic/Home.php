9b550345d09962be5a6b4a65bd0f4d88671e3635
Nazi <banonazisharifi@gmail.com>
2025-08-10 10:31:25 +0430

    Sign up form added

--- poblic/Home.php
+++ poblic/Home.php
@@ -6,9 +6,11 @@
     <title>Document</title>
     <link rel="stylesheet" href="../src/output.css">
 </head>
-<body class="h-screen w-full  bg-linear-180 from-pink-500 to-violet-950 flex justify-center items-center">
-    <div class="w-[90%] h-[90%] shadow-md shadow-gray-500 flex-col  flex justify-between">
-        <form action="" class="w-1/2 h-full flex flex-col bg-gray-200 rounded-2xl">
+<body class="h-screen w-full bg-gray-600 flex justify-center items-center">
+    <div class="w-[90%] h-[90%] shadow-md shadow-gray-500  bg-gray-900 ">
+        <h1 class="font-bold text-2xl text-center">Sign Up</h1>
+    <div class="flex justify-between h-full w-full">
+        <form action="" class="w-1/2 h-full flex flex-col justify-between py-12 rounded-2xl">
             <label for="">
                 <span class="font-bold text-xl">Name</span>
             <input type="text" class="shadow-md shadow-gray-500 border rounded-xl w-full py-3">
@@ -23,14 +25,16 @@
             </label>
             <label for="">
                 <span class="font-bold text-xl">Confirm Password</span>
-            <input type="password" class="shadow-md shadow-gray-500 border rounded-xl  w-full py-3">
+            <input type="password" class="shadow-md shadow-gray-500  rounded-xl  w-full py-3">
             </label>
             <div class="w-full flex justify-between font-bold text-xs">
                 <span>Forgit Password?</span>
                 <span>Sign In</span>
             </div>
+        <button class="py-3 px-4 mx-auto rounded-2xl rounded-t-none font-bold text-xl w-[40%] bg-violet-950 shdow-md shadow-gray-500">Sing Up</button>
         </form>
-        <button class="py-3 px-4 rounded-2xl rounded-t-none font-bold text-xl w-[40%] bg-violet-950 shdow-md shadow-gray-500">Sing Up</button>
+        <img src="./img/download (1).jpg" alt="">
+    </div>
     </div>
 </body>
 </html>
