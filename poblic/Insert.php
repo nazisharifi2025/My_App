2da81040865aaed24bce0a0331ee20f5a447d922
Nazi <banonazisharifi@gmail.com>
2025-08-11 22:22:43 +0430

    Some Styel added

--- poblic/Insert.php
+++ poblic/Insert.php
@@ -9,7 +9,7 @@
 <body class="h-screen w-full bg-gray-500 text-white flex justify-center items-center font-bold text-2xl">
     <div class="w-[40%] h-[80%] border rounded-2xl bg-gray-700 p-4 text-white font-bold text-center flex flex-col gap-2.5 justify-center items-center">
         <h1 class="font-bold text-xl">Sign in</h1>
-        <input type="text" class="w-full py-3 px-4">
+        <input type="text" class="w-full py-3 px-4 border border-gray-500">
     </div>
 </body>
 </html>
