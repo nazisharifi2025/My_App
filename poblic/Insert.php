b507d8b132c7a7d3a11d288678442969e22efcaf
Nazi <banonazisharifi@gmail.com>
2025-08-11 22:17:14 +0430

    Some Styel added

--- poblic/Insert.php
+++ poblic/Insert.php
@@ -7,6 +7,6 @@
     <link rel="stylesheet" href="../src/output.css">
 </head>
 <body class="h-screen w-full bg-gray-500 text-white flex justify-center items-center font-bold text-2xl">
-    <div class="w-[40%] h-[80%] border rounded-2xl bg-gray-700 text-white font-bold text-center"></div>
+    <div class="w-[40%] h-[80%] border rounded-2xl bg-gray-700 text-white font-bold text-center flex flex-col gap-2.5 justify-center items-center"></div>
 </body>
 </html>
