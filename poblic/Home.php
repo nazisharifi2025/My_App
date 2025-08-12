4914dcc68cb702ee7a786058d9a3aaeb34977a02
Nazi <banonazisharifi@gmail.com>
2025-08-12 12:25:32 +0430

    Some Styel added

--- poblic/Home.php
+++ poblic/Home.php
@@ -37,8 +37,8 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     <title>Document</title>
     <link rel="stylesheet" href="../src/output.css">
 </head>
-<body class="h-screen w-full bg-linear-180 text-white flex justify-center items-center">
-    <div class="w-[35%] mx-auto h-[85%] relative shadow-md shadow-gray-500  bg-gray-900 ">
+<body class="h-screen w-full bg-gradient-to-tl from-pink-300 to-gray-700 via-gray-300 text-white flex py-4">
+    <div class="w-[35%] mx-auto h-[90%] relative shadow-2xl shadow-gray-500 rounded-xl  bg-gray-300 my-3">
         <h1 class="font-bold text-3xl my-4 text-center">Sign Up</h1>
     <!-- <div class="flex justify-between h-full w-full"> -->
         <form action=<?php echo $_SERVER["PHP_SELF"] ?> method="post" class="w-[90%] mx-auto h-full gap-2.5 flex flex-col  rounded-2xl">
@@ -60,13 +60,13 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             </label>
             <label for="">
                 <span class="font-bold text-xl">Confirm Password</span>
-            <input type="password" name="password2" class="shadow-md shadow-gray-500  rounded-xl  w-full py-3">
+            <input type="password" name="password2" class="shadow-md shadow-gray-500 border  rounded-xl  w-full py-3">
             </label>
             <div class="w-full flex justify-between font-bold text-[16px]">
                 <span class="hover:underline hover:cursor-pointer">Forgit Password?</span>
                 <span class="hover:underline hover:cursor-pointer">Sign In</span>
             </div>
-        <button class="py-3 px-4 mx-auto rounded-2xl rounded-t-none font-bold text-xl absolute -bottom-13 left-35 hover:shadow-xs  w-[40%] bg-gray-900 shadow-md shadow-white">Sing Up</button>
+        <button class="py-3 px-8 mx-auto rounded-2xl rounded-t-none font-bold text-xl absolute -bottom-[53px] left-35 hover:shadow-xs  w-[40%] bg-gray-300 shadow-md shadow-gray-600">Sing Up</button>
         </form>
         <!-- <img src="./img/download (1).jpg" alt=""> -->
     </div>
--- poblic/signin.php
+++ poblic/signin.php
@@ -11,13 +11,23 @@ if(isset($_GET['err'])){
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="../src/output.css">
+    <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" rel="stylesheet" />
 </head>
 <body class="h-screen w-full bg-gray-100 text-2xl text-black flex justify-center items-center flex-col">
-    <div class="w-1/2 h-[80%] border border-gray-400 rounded-2xl shadow-md shadow-gray-600 flex flex-col gap-2.5 p-4 justify-between">
+    <div class="w-[35%] h-[60%] border border-gray-400 rounded-2xl px-8 items-center shadow-md shadow-gray-600 flex flex-col gap-2.5 p-4 justify-center">
         <h1 class="font-bold text-xl text-center">Login Page</h1>
-        <input type="text" class="px-2 py-2 border border-gray-600 bg-gray-100 font-bold">
-        <input type="text" class="px-2 py-2 border border-gray-600 bg-gray-100 font-bold">
-        <input type="text" class="px-2 py-2 border border-gray-600 bg-gray-100 font-bold">
+        <div class="flex w-full">
+         <span class="material-symbols-outlined p-3 bg-gray-400">
+          person
+         </span>
+        <input type="text" class="px-2 py-2 bg-gray-300 outline-0">
+        </div>
+        <div class="flex w-full">
+            <span class="material-symbols-outlined p-3 bg-gray-400">
+lock
+</span>
+        <input type="text" class="px-2 py-2  bg-gray-300  outline-0">
+</div>
     </div>
 </body>
 </html>
