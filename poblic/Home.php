c3c8c63025ed9f102930a7dc1238044ed2f627f5
Nazi <banonazisharifi@gmail.com>
2025-08-15 19:14:26 +0430

    some style for nav added

--- poblic/Home.php
+++ poblic/Home.php
@@ -22,7 +22,7 @@ else{
     <link rel="stylesheet" href="../src/output.css">
 </head>
 <body>
-    <nav class="h-20 w-full"></nav>
+    <nav class="h-20 w-full rounded-2xl rounded-t-none"></nav>
 </body>
 </html>
 <?php } ?>
